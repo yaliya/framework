992f0c857345d9d74752960224161d052c83a006
Yusuf Alija <yaliya@Yusufs-MBP.speedport.ip>
2017-03-18 22:19:24 +0100

    Session started bug

--- src/Session.php
+++ src/Session.php
@@ -4,33 +4,38 @@ namespace Tau;
 
 class Session
 {
+  public static function start() {
+    if(session_status() == PHP_SESSION_NONE) {
+      session_start();
+    }
+  }
   public static function get($name) {
-    session_start();
+    self::start();
     return $_SESSION[$name];
   }
 
   public static function save($name, $value) {
-    session_start();
+    self::start();
     $_SESSION[$name] = $value;
   }
 
   public static function set($name) {
-    session_start();
+    self::start();
     return isset($_SESSION[$name]);
   }
 
   public static function all() {
-    session_start();
+    self::start();
     return $_SESSION;
   }
 
   public static function remove($name) {
-    session_start();
+    self::start();
     unset($_SESSION[$name]);
   }
 
   public static function clear() {
-    session_start();
+    self::start();
     session_destroy();
   }
 }
